6b0b66f81314634fc28d7fb6a69d775f1216bba7
minhtri09 <35910263+minhtri09@users.noreply.github.com>
2025-04-28 22:33:48 +0700

    Update home.js

--- langgeek.net/src/home.js
+++ langgeek.net/src/home.js
@@ -1,14 +1,6 @@
 function execute() {
     return Response.success([
-        {
-            title: "Mới cập nhật",
-            input: "https://langgeek.net/truyen-moi-cap-nhat/",
-            script: "homecontent.js"
-        },
-        {
-            title: "Top truyện",
-            input: "https://langgeek.net/top-truyen/",
-            script: "homecontent.js"
-        }
+        { title: "Mới cập nhật", input: "https://langgeek.net/truyen-moi-cap-nhat", script: "gen.js" },
+        { title: "Top truyện", input: "https://langgeek.net/top-truyen", script: "gen.js" }
     ]);
 }
