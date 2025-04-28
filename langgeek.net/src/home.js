3de1da6b8c90753679a06a2720f3b9b0a5807875
minhtri09 <35910263+minhtri09@users.noreply.github.com>
2025-04-28 23:26:34 +0700

    Update home.js

--- langgeek.net/src/home.js
+++ langgeek.net/src/home.js
@@ -1,9 +1,20 @@
 function execute() {
+    Console.log("Executing home.js");
     return Response.success([
         { 
             title: "Tất cả truyện", 
             input: "https://langgeek.net",
             script: "gen.js" 
+        },
+        {
+            title: "All Star Superman",
+            input: "https://langgeek.net/all-star-superman",
+            script: "gen.js"
+        },
+        {
+            title: "DC All In Special",
+            input: "https://langgeek.net/dc-all-in-special-2024",
+            script: "gen.js"
         }
     ]);
 }
