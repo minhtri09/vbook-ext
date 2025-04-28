42f766644cf4f2404ef6fd5be286fd7e4be11669
minhtri09 <35910263+minhtri09@users.noreply.github.com>
2025-04-28 22:48:57 +0700

    Update home.js

--- langgeek.net/src/home.js
+++ langgeek.net/src/home.js
@@ -1,6 +1,14 @@
 function execute() {
     return Response.success([
-        { title: "Mới cập nhật", input: "https://langgeek.net/truyen-moi-cap-nhat", script: "gen.js" },
-        { title: "Top truyện", input: "https://langgeek.net/top-truyen", script: "gen.js" }
+        { 
+            title: "Mới cập nhật", 
+            input: "https://langgeek.net", // Changed to main page since latest updates are on homepage
+            script: "gen.js" 
+        },
+        { 
+            title: "Top truyện", 
+            input: "https://langgeek.net/top", // Updated URL for top novels
+            script: "gen.js" 
+        }
     ]);
 }
