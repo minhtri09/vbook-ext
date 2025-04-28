284f36fc6ff4b7983b167ed44d371dc6317f4e18
minhtri09 <35910263+minhtri09@users.noreply.github.com>
2025-04-28 22:35:48 +0700

    Update gen.js

--- langgeek.net/src/gen.js
+++ langgeek.net/src/gen.js
@@ -1,22 +1,29 @@
 function execute(url, page) {
     if (!page) page = '1';
-    let response = fetch('https://langgeek.net/'+url+'_lastupdate_0_0_0_0_0_0_'+page+'.html')
-    if(response.ok){
-        let doc = response.html('gbk');
-        var next = doc.select(".page").select("strong + a").text()
-        const el = doc.select(".listRightBottom ul li")
-        const data = [];
-        for (var i = 0; i < el.size(); i++) {
-            var e = el.get(i);
-            data.push({
-                name: e.select("h2 a").first().text(),
-                link: e.select("h2 a").first().attr("href"),
-                cover: e.select("img").first().attr("src"),
-                description: e.select(".writer").first().text(),
-                host: "https://langgeek.net/"
-            })
+    
+    let response = fetch(url + (page == '1' ? '' : '/page/' + page));
+    if (response.ok) {
+        let doc = response.html();
+        let novels = [];
+        
+        // Select all novel items
+        doc.select("div.item").forEach(e => {
+            novels.push({
+                name: e.select("h3.title a").text(),
+                link: e.select("h3.title a").attr("href"),
+                cover: e.select("img.book-cover").attr("src"),
+                description: e.select("div.excerpt").text()
+            });
+        });
+
+        // Get next page
+        let next = doc.select("div.pagination a.next").attr("href");
+        let nextPage = null;
+        if (next) {
+            nextPage = (parseInt(page) + 1).toString();
         }
-        return Response.success(data, next)
+
+        return Response.success(novels, nextPage);
     }
-    return null
+    return Response.error("Không thể tải trang");
 }
