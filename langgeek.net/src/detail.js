2ccb7fb54dc7d77e22c3dc64d0cdab61280e77da
minhtri09 <35910263+minhtri09@users.noreply.github.com>
2025-04-28 22:36:24 +0700

    Update detail.js

--- langgeek.net/src/detail.js
+++ langgeek.net/src/detail.js
@@ -1,25 +1,16 @@
 function execute(url) {
     let response = fetch(url);
-    if (!response.ok) return Response.error("Failed to fetch detail page.");
-
-    let doc = response.html();
-    let name = doc.select("h1.title").text();
-    let cover = doc.select(".cover img").attr("src");
-    let author = doc.select(".author a").text();
-    let description = doc.select(".description").text();
-    let ongoing = doc.select(".status").text().toLowerCase().includes("ongoing");
-    let genres = doc.select(".genres a").toArray().map(genre => ({
-        title: genre.text(),
-        input: genre.attr("href"),
-        script: "genre.js"
-    }));
-
-    return Response.success({
-        name: name,
-        cover: cover,
-        author: author,
-        description: description,
-        ongoing: ongoing,
-        genres: genres
-    });
+    if (response.ok) {
+        let doc = response.html();
+        
+        return Response.success({
+            name: doc.select("h1.entry-title").text(),
+            cover: doc.select("div.book-thumbnail img").attr("src"),
+            author: doc.select("div.book-information div:contains(Tác giả:) span").text(),
+            description: doc.select("div.desc-text").text(),
+            detail: doc.select("div.book-information").text(),
+            ongoing: doc.select("div.book-information").text().includes("Đang ra")
+        });
+    }
+    return Response.error("Không thể tải thông tin truyện");
 }
