c0d01f183c6a965f66e90e320606f4bc941342a1
Nguyen Hai Dong <35910263+minhtri09@users.noreply.github.com>
2025-05-08 12:17:32 +0700

    Update home.js

--- langgeek.net/src/home.js
+++ langgeek.net/src/home.js
@@ -1,9 +1,5 @@
 function execute() {
     return Response.success([
-        {
-            title: "Truyện mới cập nhật",
-            input: "https://langgeek.net/",
-            script: "gen.js"
-        }
+        {title: "Truyện ngẫu nhiên", input: "https://langgeek.net/", script: "homecontent.js"}
     ]);
 }
