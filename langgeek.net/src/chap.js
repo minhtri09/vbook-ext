c407cc6a4788e4ddc1f90d1916994914bcb5c8e5
minhtri09 <35910263+minhtri09@users.noreply.github.com>
2025-05-09 11:08:27 +0700

    Xóa folder langgeek.net

--- langgeek.net/src/chap.js
+++
@@ -1,6 +0,0 @@
-function execute(url) {
-    let doc = Http.get(url).html();
-    let imgs = [];
-    doc.select('img').forEach(e => imgs.push(e.attr('src')));
-    return Response.success(imgs);
-}
