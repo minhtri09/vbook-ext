631af3eafe71256ebfc8dbdc9abc159ac7d4e8a6
minhtri09 <35910263+minhtri09@users.noreply.github.com>
2025-05-09 10:34:58 +0700

    Xóa folder langgeek.net

--- langgeek.net/src/home.js
+++
@@ -1,7 +0,0 @@
-load('config.js');
-function execute() {
-    return Response.success([
-        {title: "Truyện mới cập nhật", input: BASE_URL + "truyen-moi-cap-nhat", script: "gen.js"},
-        {title: "Top truyện", input: BASE_URL + "top-truyen", script: "gen.js"},
-    ]);
-}
