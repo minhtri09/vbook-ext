f5062ca17a95b9804e4f7f69e887bafae01998da
minhtri09 <35910263+minhtri09@users.noreply.github.com>
2025-05-09 08:36:16 +0700

    Xóa file chap.js

--- chap.js
+++
@@ -1,9 +0,0 @@
-function execute(url) {
-    let response = fetch(url);
-    if (!response.ok) return Response.error("Failed to fetch chapter.");
-
-    let doc = response.html();
-    let content = doc.select(".chapter-content").html();
-
-    return Response.success(content);
-}
